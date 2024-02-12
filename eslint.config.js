6da77d7dd86d0a0478357613098a05b9309681b7
SukkaW <isukkaw@gmail.com>
2024-02-12 23:18:53 +0800

    Fix build

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 
 module.exports = require('eslint-config-sukka').sukka({
   node: {
-    files: ['eslint.config.js', './build/webpack.config.js']
+    files: ['eslint.config.js']
   },
   ts: {},
   react: true
